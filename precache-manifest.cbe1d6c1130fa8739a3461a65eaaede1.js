a338a7ff17e099386ad0d36a2a0ca7733c09a2c2
Vrushabacharya <vrushabacharya1@gmail.com>
2023-09-30 12:30:56 +0530

    Updates

---
+++ precache-manifest.cbe1d6c1130fa8739a3461a65eaaede1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "174f0c23bbe1cddcc56b",
+    "url": "/NewsMonkey/static/css/main.d65de6dd.chunk.css"
+  },
+  {
+    "revision": "174f0c23bbe1cddcc56b",
+    "url": "/NewsMonkey/static/js/main.174f0c23.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/NewsMonkey/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "b920157cc74cd4018a3b",
+    "url": "/NewsMonkey/static/js/2.b920157c.chunk.js"
+  },
+  {
+    "revision": "99e07e25e5dacb379dc7",
+    "url": "/NewsMonkey/static/js/runtime~main.99e07e25.js"
+  },
+  {
+    "revision": "90fd573d05b557fbfd0416e7097a5856",
+    "url": "/NewsMonkey/index.html"
+  }
+];
